808fe16455633e8baeee11f267faa539f8f4871d
MasayaAoyama <amsy810@gmail.com>
2020-12-17 16:34:45 +0900

    kubebuilder create webhook --group servers --version v1beta1 --kind WebServer --defaulting --programmatic-validation

--- main.go
+++ main.go
@@ -75,6 +75,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "WebServer")
 		os.Exit(1)
 	}
+	if err = (&serversv1beta1.WebServer{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "WebServer")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
